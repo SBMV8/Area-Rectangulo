82d8d4cc438d15b4a5cfbaad7efe5b2ff57b02e3
Shirley Martínez <97803763+SBMV8@users.noreply.github.com>
2022-11-23 17:01:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+  PROBLEMA 12:
+  Realiza una función llamada area_rectangulo(base, altura) que devuelva el área     del rectángulo a partir de una base y una altura.
+"""
+
+print("Ingrese la altura del rectangulo")
+Altura=int(input())
+
+print("Ingrese la base del rectangulo")
+Base=int(input())
+
+Area_Rectangulo=Base*Altura
+
+print("El Area del Rectangulo es: ",Area_Rectangulo)
